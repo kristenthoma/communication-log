06ef5f2f9178d32c2d8b55653de9d9b9698d301f
Jason Post <jsnpost@gmail.com>
2021-06-22 15:23:13 -0400

    I hope this doesn't break

--- communication-log/communication-log/Program.cs
+++ communication-log/communication-log/Program.cs
@@ -11,12 +11,13 @@ namespace communication_log
             Console.WriteLine("Hello Pilot!");
             Console.WriteLine("Hello Control!");
             Console.WriteLine("Hope you had a good bathroom break");
-            Console.BackgroundColor = ConsoleColor.Pink;
+            Console.BackgroundColor = ConsoleColor.Green;
             Console.WriteLine("Yes, I can breate again");
             Console.WriteLine("Now i have to pee");
             Console.WriteLine("At least you're not in a space suit");
             Console.WriteLine("Changed your color from red to blue so we can have a merge conflict yay");
 
+            Console.WriteLine("Why did the chicken cross the road?");
 
         }
 
