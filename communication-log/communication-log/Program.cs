18489c511faf00ceb48af0a5b9685d2fc23b5866
Jason Post <jsnpost@gmail.com>
2021-06-22 14:54:36 -0400

    joke time

--- communication-log/communication-log/Program.cs
+++ communication-log/communication-log/Program.cs
@@ -10,9 +10,11 @@ namespace communication_log
             Console.WriteLine("Hello Pilot!");
             Console.WriteLine("Hello Control!");
             Console.WriteLine("Hope you had a good bathroom break");
-            Console.WriteLine("Yes, I can breate again");
+            Console.WriteLine("Yes, I can breahte again");
             Console.WriteLine("Now i have to pee");
             Console.WriteLine("At least you're not in a space suit");
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.WriteLine("Why did the chicken cross the road?");
         }
 
         
