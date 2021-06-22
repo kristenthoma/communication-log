9bd235603596a4642d9c5a6a03e17ae2f57842c3
Kristen Thoma <kristenthoma@gmail.com>
2021-06-22 12:56:12 -0400

    Added pink to branch

--- communication-log/communication-log/Program.cs
+++ communication-log/communication-log/Program.cs
@@ -11,6 +11,7 @@ namespace communication_log
             Console.WriteLine("Hello Pilot!");
             Console.WriteLine("Hello Control!");
             Console.WriteLine("Hope you had a good bathroom break");
+            Console.BackgroundColor = ConsoleColor.Pink;
             Console.WriteLine("Yes, I can breate again");
             Console.WriteLine("Now i have to pee");
             Console.WriteLine("At least you're not in a space suit");
